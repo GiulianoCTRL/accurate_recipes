4721917f4d8f7f03f91e4eec5eee68f977f75099
GiulianoCTRL <35188493+GiulianoCTRL@users.noreply.github.com>
2024-10-04 13:15:43 +0200

    Added rust-toolchain file and tested more

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
-use std::path::Path;
 
-use iced::widget::{button, column, container, row, text, text_input, Column, Container, Row};
-use iced::{Center, Element, Fill};
+use iced::widget::{button, column, container, row, text, text_input, Row};
+use iced::{Bottom, Center, Element, Fill, Top};
 
 pub fn main() -> iced::Result {
     iced::run(
-        "A cool counter",
+        "AccurateRecipe",
         AccurateRecipe::update,
         AccurateRecipe::view,
     )
@@ -15,12 +14,14 @@ pub fn main() -> iced::Result {
 #[derive(Default)]
 struct AccurateRecipe {
     value: i64,
-    search_content: String,
+    input_value: String,
+    recipes: Vec<Recipe>,
 }
 
 #[derive(Default, Clone, Debug)]
 struct Recipe {
     name: String,
+    portions: u32,
     ingredients: HashMap<String, f64>,
     instructions: Vec<String>,
     picture: String,
@@ -38,27 +39,28 @@ impl AccurateRecipe {
     fn update(&mut self, message: Message) {
         match message {
             Message::Previous => {
-                self.value += 1;
-            }
-            Message::Next => {
                 self.value -= 1;
             }
-            Message::InputChanged(content) => {
-                self.search_content = content;
+            Message::Next => {
+                self.value += 1;
+            }
+            Message::InputChanged(value) => {
+                self.input_value = value;
             }
             Message::Search => {
-                todo!("Search through recipe hashmap");
+                println!("Search through recipe hashmap");
+                self.input_value.clear();
             }
         }
     }
 
     fn view(&self) -> Element<Message> {
         let nav_bar: Row<Message> = row![
-            button("<-").on_press(Message::Previous),
-            text_input("search", "")
+            button("←").on_press(Message::Previous),
+            text_input("search", &self.input_value)
                 .on_input(Message::InputChanged)
                 .on_submit(Message::Search),
-            button("->").on_press(Message::Next)
+            button("→").on_press(Message::Next)
         ];
 
         let body_placeholder: Row<Message> =
@@ -73,15 +75,36 @@ impl AccurateRecipe {
             button(">>").on_press(Message::Next)
         ];
 
-        container(row![
-            column![nav_bar, body_placeholder, footer_placeholder]
-                .padding(20)
-                .align_x(Center),
-            row!["Left", "Right"].padding(20).align_y(Center),
-        ])
+        container(row![column![
+            nav_bar.align_y(Top).padding(50),
+            body_placeholder.align_y(Center).padding(100),
+            footer_placeholder.align_y(Bottom)
+        ]
+        .padding(20)
+        .align_x(Center),])
         .padding(10)
         .center_x(Fill)
         .center_y(Fill)
         .into()
     }
+}
+
+#[test]
+fn it_counts_properly() {
+    let recipes = vec![Recipe {
+        name: String::from("test"),
+        ..Default::default()
+    }];
+
+    let mut counter = AccurateRecipe {
+        value: 0,
+        input_value: String::from("Test"),
+        recipes,
+    };
+
+    counter.update(Message::Next);
+    counter.update(Message::Next);
+    counter.update(Message::Previous);
+
+    assert_eq!(counter.value, 1);
 }
