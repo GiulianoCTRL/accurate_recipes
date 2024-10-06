6a680c9ca06e97c648d73e9555269cda21fba9fc
GiulianoCTRL <35188493+GiulianoCTRL@users.noreply.github.com>
2024-10-06 15:01:03 +0200

    Replacing placeholder with real values, adding tests

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,49 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
 
 use iced::widget::{button, column, container, image, row, text, text_input, Row};
-use iced::{Bottom, Center, Element, Fill, Settings, Top};
+use iced::{Bottom, Center, Element, Fill, Task, Top};
 use serde::{Deserialize, Serialize};
 
 const APP_NAME: &str = "AccurateRecipe";
+const RECIPE_FILE: &str = "recipes.json";
 
 pub fn main() -> iced::Result {
+    // Add recipe.json creation if not existend to main file
     iced::application(APP_NAME, AccurateRecipe::update, AccurateRecipe::view)
-        .settings(Settings {
-            id: Some(String::from(APP_NAME)),
-            antialiasing: true,
-            ..Settings::default()
-        })
-        .antialiasing(true)
-        .centered()
-        .run()
+        .run_with(AccurateRecipe::new)
+}
+
+#[derive(Default, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+struct Recipe {
+    name: String,
+    portions: u32,
+    ingredients: HashMap<String, u32>,
+    instructions: Vec<String>,
+    image: String,
+}
+
+impl Recipe {
+    fn ingredients_to_string(&self) -> String {
+        todo!()
+    }
+    fn instruction_to_string(&self) -> String {
+        todo!()
+    }
+}
+fn recipes_from_file(filename: &str) -> Result<Vec<Recipe>, std::io::Error> {
+    let recipes: Vec<Recipe> = serde_json::from_reader(BufReader::new(File::open(filename)?))?;
+    Ok(recipes)
 }
 
 #[derive(Default)]
 struct AccurateRecipe {
-    value: i64,
+    page: usize,
     input_value: String,
     recipes: Vec<Recipe>,
-}
-
-#[derive(Default, Clone, Debug, Deserialize, Serialize)]
-struct Recipe {
-    // todo add #[serde(rename)] and new() for application that readsfile path
-    name: String,
-    portions: u32,
-    ingredients: HashMap<String, f64>,
-    instructions: Vec<String>,
-    image: String,
 }
 
 #[derive(Debug, Clone)]
@@ -44,13 +55,28 @@ enum Message {
 }
 
 impl AccurateRecipe {
+    fn new() -> (AccurateRecipe, Task<Message>) {
+        (
+            AccurateRecipe {
+                page: 0,
+                input_value: String::from(""),
+                recipes: recipes_from_file(RECIPE_FILE).unwrap(),
+            },
+            Task::none(),
+        )
+    }
+
     fn update(&mut self, message: Message) {
         match message {
             Message::Previous => {
-                self.value -= 1;
+                if self.page > 0 {
+                    self.page -= 1;
+                }
             }
             Message::Next => {
-                self.value += 1;
+                if self.page < self.recipes.len() {
+                    self.page += 1;
+                }
             }
             Message::InputChanged(value) => {
                 self.input_value = value;
@@ -73,19 +99,33 @@ impl AccurateRecipe {
 
         let body_placeholder: Row<Message> = row![
             column![
-                container("Ingredients").width(Fill).height(Fill),
-                container("Instructions").width(Fill).height(Fill)
+                container(column!["Ingredients", "lorem ipsum"])
+                    .width(Fill)
+                    .height(Fill),
+                container(column!["Instructions", "lorem ipsum"])
+                    .width(Fill)
+                    .height(Fill)
             ],
-            image("placeholder.jpg").width(Fill)
+            image(&self.recipes[self.page].image).width(Fill)
         ];
 
+        let previous_page = if self.page > 0 {
+            (self.page - 1).to_string()
+        } else {
+            0.to_string()
+        };
+        let next_page = if self.page == self.recipes.len() {
+            "end".to_string()
+        } else {
+            (self.page + 1).to_string()
+        };
         let footer_placeholder: Row<Message> = row![
-            text("0"),
-            text("Recipe Name")
+            text(previous_page),
+            text(&self.recipes[self.page].name)
                 .align_x(Center)
                 .align_y(Center)
                 .width(Fill),
-            text("2")
+            text(next_page)
         ];
 
         container(row![column![
@@ -110,7 +150,7 @@ fn it_counts_properly() {
     }];
 
     let mut counter = AccurateRecipe {
-        value: 0,
+        page: 0,
         input_value: String::from("Test"),
         recipes,
     };
@@ -119,5 +159,23 @@ fn it_counts_properly() {
     counter.update(Message::Next);
     counter.update(Message::Previous);
 
-    assert_eq!(counter.value, 1);
+    assert_eq!(counter.page, 1);
+}
+
+#[test]
+fn recipes_read_correctly() {
+    let filename = "test.json";
+    let mut ingredients: HashMap<String, u32> = HashMap::new();
+    ingredients.insert("test_ingredient".to_string(), 1);
+
+    let expected = vec![Recipe {
+        name: "test".to_string(),
+        portions: 1,
+        ingredients,
+        instructions: vec!["Do not eat.".to_string()],
+        image: "test.jpg".to_string(),
+    }];
+
+    let result = recipes_from_file(filename).unwrap();
+    assert_eq!(expected[0], result[0]);
 }
